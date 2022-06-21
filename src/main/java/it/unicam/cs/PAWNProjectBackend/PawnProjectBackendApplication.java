e8d597d95104fbe99171d0f5fe015be61b0b8732
TiburzioFoglia <simone.debernardinis@studenti.unicam.it>
2022-06-21 11:06:25 +0200

    init
    init

---
+++ src/main/java/it/unicam/cs/PAWNProjectBackend/PawnProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package it.unicam.cs.PAWNProjectBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawnProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PawnProjectBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/unicam/cs/PAWNProjectBackend/PawnProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unicam.cs.PAWNProjectBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawnProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
